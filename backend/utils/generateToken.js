e4d469aaca0d8ad3468a4e9c66222ccabbcd84d2
NelloreEswaraiahBharath <eswaraiahbharath@gmail.com>
2025-01-28 12:14:09 +0530

    final deploy

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const generateTokenAndSetCookie = (userId, res) => {
-	const token = jwt.sign({ userId }, "998566", {
+	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
 		expiresIn: "15d",
 	});
 
